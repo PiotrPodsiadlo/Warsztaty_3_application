f883a0e6e5d32fbeb0d354cd56f79f9030273670
Piotr <piotrekchorzow@interia.eu>
2019-06-30 20:35:30 +0200

    task4 - zrobione pojawianie się diva

--- Application/js/app.js
+++ Application/js/app.js
@@ -5,7 +5,7 @@ $(function(){
         method: 'GET'
     }).done(function (response) {
         renderTitles(response);
-        console.log(response);
+
 
 
     }).fail(function(){
@@ -18,9 +18,27 @@ divApp = $("#app");
 
 renderTitles = function(resp) {
     Object.values(resp).forEach(function (obj) {                // biore wartości ze zmienna odpowiedzi response (która jest obiektem i dla kazdego jej elementu dodaję paragraf z tą wartością do html
-
-        let newP = $(`<p>Tytuł książki ${obj.id} to <h3 id="book-title">"${obj.title}"</h3></p><div id="info"></div>`);
+        let newP = $(`<p>Tytuł książki ${obj.id} to</p> <h3 class="book-title">"${obj.title}"<div class="info"></div></h3>`);
         divApp.append(newP)
     })
 
+
+    let h3Titles = document.querySelectorAll(".book-title");
+    // let infoDivs = $('.info')
+    h3Titles.forEach(function (elem) {
+        console.log(elem);
+        $(elem).on("click", function(event){
+            let thisDiv = $(this).find("div");
+            // $(thisDiv).css("visibility", "visible");
+            $(thisDiv).fadeIn("slow");
+            console.log(thisDiv);
+        })
+    })
+
+
+
+
 }
+
+
+
